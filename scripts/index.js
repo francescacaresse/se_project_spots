46c5fc232845ef8bf9a1aea265326b7841edd879
Francesca Curry <francescacaresse@gmail.com>
2025-04-29 22:04:15 -0400

    modal close button adjusted to figma

--- scripts/index.js
+++ scripts/index.js
@@ -104,6 +104,7 @@ function getCardElement(data) {
 
   cardImageEl.addEventListener("click", () => {
     previewImageEl.src = data.link;
+    previewImageEl.alt = data.name;
     previewCaptionEl.textContent = data.name;
     openModal(previewModal);
   });
